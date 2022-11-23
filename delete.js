e06bd7f42c80d2c8903fc50cadaa58b67f3d0e6e
asif-karim100 <asif.karim100@gmail.com>
2022-11-24 02:18:12 +0530

    filter

--- delete.js
+++ delete.js
@@ -15,6 +15,7 @@ function addItem(e){
 
   // Get input value
   var newItem = document.getElementById('item').value;
+  var descriptionitem = document.getElementById('description').value;
 
   // Create new li element
   var li = document.createElement('li');
@@ -22,6 +23,7 @@ function addItem(e){
   li.className = 'list-group-item';
   // Add text node with input value
   li.appendChild(document.createTextNode(newItem));
+  li.appendChild(document.createTextNode(descriptionitem));
 
   // Create del button element
   var deleteBtn = document.createElement('button');
@@ -64,5 +66,23 @@ function removeItem(e){
       itemList.removeChild(li);
     }
   }
+}
+
+    // Filter Items
+function filterItems(e){
+  // convert text to lowercase
+  var text = e.target.value.toLowerCase();
+  // Get lis
+  var items = itemList.getElementsByTagName('li');
+  // Convert to an array
+  Array.from(items).forEach(function(item){
+    var itemName = item.firstChild.textContent; 
+    var description = item.childNodes[1].textContent;
+    if(itemName.toLowerCase().indexOf(text) != -1 || description.toLowerCase().indexOf(text) != -1){
+      item.style.display = 'block';
+    } else {
+      item.style.display = 'none';
+    }
+  });
 }
 
